29b6b1dac975888d849d17d310cb07914dab4b6f
andreitotoro <andreitotoro@gmail.com>
2024-10-16 10:44:59 +0300

    Finnish lab2

--- lab1/solana-training/generate-keypair.ts
+++ lab1/solana-training/generate-keypair.ts
@@ -1,8 +1,10 @@
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 import { Keypair } from "@solana/web3.js";
 import "dotenv/config";
+import bs58 from "bs58";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
 console.log("Public key loaded from a private key stored in the env file!");
 console.log(keypair.publicKey.toBase58());
+console.log(bs58.encode(keypair.secretKey));
