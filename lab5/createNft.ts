6d9af822aaa83a546bbe122176ba57f4c242f055
andreitotoro <andreitotoro@gmail.com>
2024-11-07 15:16:01 +0200

    Finnish lab 5

---
+++ lab5/createNft.ts
@@ -0,0 +1,19 @@
+import { Metaplex } from "@metaplex-foundation/js";
+
+export async function createNft(metaplex: Metaplex, uri: string, nftData: any) {
+  const { nft } = await metaplex.nfts().create(
+    {
+      uri: uri,
+      name: nftData.name,
+      sellerFeeBasisPoints: 500,
+      symbol: nftData.symbol,
+    },
+    { commitment: "finalized" }
+  );
+
+  console.log(
+    `Created NFT: https://explorer.solana.com/address/${nft.address.toBase58()}?cluster=devnet`
+  );
+
+  return nft;
+}
